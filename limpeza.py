7484bc23792f53abb8c23a50fff0c12647f1a84c
Lucival Junior <40046044+lucival-junior@users.noreply.github.com>
2020-10-23 19:31:00 -0300

    Update limpeza.py

--- limpeza.py
+++ limpeza.py
@@ -1,6 +1,7 @@
 #variaveis de arquivo saida
 primeira_limpeza = "primeira_limpeza.txt"
 segunda_limpeza = "segunda_limpeza.txt"
+terceira_limpeza = "terceira_limpeza.txt"
 
 def limpeza_sococo():
     busca_sococo_cod = '21'
@@ -24,6 +25,8 @@ def limpeza_sococo():
 def limpeza_acqua():
     busca_acqua_cod = '40'
     busca_acqua_emp = "ACQUA"
+     busca_acqua_valor = "Valor"
+    
     with open(primeira_limpeza, 'w', encoding="ISO-8859-1") as out_f:
         with open('arquivo_sem_tratamento.txt', "r", encoding='ISO-8859-1') as in_f:
             for line in in_f:
@@ -36,6 +39,13 @@ def limpeza_acqua():
         with open('primeira_limpeza.txt', "r", encoding="ISO-8859-1") as in_f:
             for line in in_f:
                 if busca_acqua_emp in line:
+                    pass
+                else:
+                    out_f.write(line)
+    with open(terceira_limpeza, 'w', encoding="ISO-8859-1") as out_f:
+        with open('segunda_limpeza.txt', "r", encoding="ISO-8859-1") as in_f:
+            for line in in_f:
+                if busca_acqua_valor in line:
                     pass
                 else:
                     out_f.write(line)
